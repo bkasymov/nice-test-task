2d8a95977010e9a7e0c134c447f6372848c34652
Бехруз Касымов <bkasymovwgt@wegotrip.com>
2024-09-06 05:19:09 +0200

    add k8s manifests and md doc

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-// Handler responds with a greeting message.
-// It writes "Hello from PSPDFKit Engineer!" to the response writer.
 func Handler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Hello from PSPDFKit Engineer!")
 }
